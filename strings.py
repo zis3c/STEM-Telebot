5c03686a9e3307df8fae2d3cba48fb755f65baf8
RADZ <radz@example.com>
2026-04-04 00:30:13 +0800

    Tweak copy in strings

--- strings.py
+++ strings.py
@@ -21,7 +21,6 @@ STRINGS = {
         'HELP_MSG': (
             "*About This Bot*\n\n"
             "This bot checks the STEM USAS membership database. It connects securely to the official records.\n\n"
-            "If you face any problems, please contact @STEMUSAS\n\n"
             "Dev: zis3c ☺️"
         ),
         'HOW_IT_WORKS_MSG': (
